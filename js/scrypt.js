6e4572d0219197e9e4adc5c208623a657ba71206
Dukessa <108992189+Dukessa@users.noreply.github.com>
2023-06-11 17:25:05 +0300

    first commit

---
+++ js/scrypt.js
@@ -0,0 +1,8 @@
+
+document.querySelector('.form__btn').addEventListener('click', (e) => {
+  document.querySelector('.modal').classList.remove('modal--js');
+});
+
+document.querySelector('.button-media-428').addEventListener('click', () => {
+  document.querySelector('.button-media-428').classList.remove('button-media-428--js');
+});
